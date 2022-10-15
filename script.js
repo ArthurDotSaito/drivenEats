535b313df9a844f311dd8b17bde591958464c3d0
ArthurDotSaito <arthur_saito1998@hotmail.com>
2022-10-15 18:30:40 -0300

    Added container selection for first containers

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-//let drinksOption = '';
-//let dessertsOption = '';
-
-
-/* function selectOption(container){
-    let selectedBefore = document.querySelector('.selected');
-    if(selectedBefore !== null){selectedBefore.classList.remove('selected');}
-    console.log(container);
-    container = document.querySelector('.selected');
-    let check = document.querySelector('.check');
-
-    container.classList.toggle('selected');
-    check.classList.toggle('unhidden');
-} */
-
 function selectOption(selector){
-    console.group(selector);
-    const selectedBefore = document.querySelector('.selected')
-    if(selectedBefore !== null){selectedBefore.classList.remove('selected')}
+    const selectedBefore = document.querySelector('.selected');
+    if(selectedBefore !== null){selectedBefore.classList.remove('selected');}
     selector.classList.toggle('selected');
-}
 
-fuction showIcon(){
-    
+    const checkedBefore = document.querySelector('.unhidden');
+    if(checkedBefore !== null){checkedBefore.classList.remove('unhidden');}
+    const checked = selector.querySelector('.price> .check');
+    checked.classList.add('unhidden');
 }
