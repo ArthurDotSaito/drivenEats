6a21a00af8b7baf7ded24dada7ac4ec627c4e266
ArthurDotSaito <arthur_saito1998@hotmail.com>
2022-10-16 11:11:57 -0300

    Ending menu confirmation status

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function selectOption(selector){
-    const selectedBefore = document.querySelector('.selected');
+function selectDishes(selector){
+    const selectedBefore = document.querySelector('.prato>.selected');
     if(selectedBefore !== null){selectedBefore.classList.remove('selected');}
     selector.classList.toggle('selected');
 
-    const checkedBefore = document.querySelector('.unhidden');
+    const checkedBefore = document.querySelector('.priceOne> .unhidden');
     if(checkedBefore !== null){checkedBefore.classList.remove('unhidden');}
-    const checked = selector.querySelector('.price> .check');
+    const checked = selector.querySelector('.check');
+    checked.classList.add('unhidden');
+}
+
+function selectDrinks(selector){
+    const selectedBefore = document.querySelector('.bebidas>.selected');
+    if(selectedBefore !== null){selectedBefore.classList.remove('selected');}
+    selector.classList.toggle('selected');
+
+    const checkedBefore = document.querySelector('.priceTwo> .unhidden');
+    if(checkedBefore !== null){checkedBefore.classList.remove('unhidden');}
+    const checked = selector.querySelector('.check');
+    checked.classList.add('unhidden');
+}
+
+function selectDessert(selector){
+    const selectedBefore = document.querySelector('.sobremesa>.selected');
+    if(selectedBefore !== null){selectedBefore.classList.remove('selected');}
+    selector.classList.toggle('selected');
+
+    const checkedBefore = document.querySelector('.priceThree> .unhidden');
+    if(checkedBefore !== null){checkedBefore.classList.remove('unhidden');}
+    const checked = selector.querySelector('.check');
     checked.classList.add('unhidden');
 }
